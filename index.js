6cad0d13b4a3ec6145855ce3462695815606cc18
lokesh parmar <lokesh.parmar998@gmail.com>
2018-08-03 15:03:41 +0530

    basic setup done(assets,models,routes,views) for future cloning

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express =require('express');
+const routes =require('./routes/user');
 var app = express();
+
 /* ejs setting up*/
   app.set('view engine','ejs');
   app.use('/assests',express.static('assests'));
+
 /*body-parser */
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
 
+/*requests */
+  app.get('/',function(req,res){
+    app.render('home');
+  });
+  app.use('/user',routes.user);
 
 const port = process.env.PORT
 app.listen(port,function(port){
